e9ed6fcf420d2fa10e27823a2c382c43fe47a504
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-15 16:04:30 -0500

    setup navbar and removed more boilerplate

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from "~/utils/api";
+import Link from "next/link";
+
 export function Navbar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn-ghost btn text-xl normal-case">daisyUI</a>
+        <a className="btn-ghost btn text-xl normal-case">
+          <span className="text-[hsl(280,100%,70%)]">T3</span> Project Manager
+        </a>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown-end dropdown">
@@ -16,16 +22,13 @@ export function Navbar() {
             className="dropdown-content menu rounded-box menu-sm mt-3 w-52 bg-base-100 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
+              <Link href="/profile">Profile</Link>
             </li>
             <li>
-              <a>Settings</a>
+              <Link href="/about">About</Link>
             </li>
             <li>
-              <a>Logout</a>
+              <AuthShowcase />
             </li>
           </ul>
         </div>
@@ -33,3 +36,21 @@ export function Navbar() {
     </div>
   );
 }
+
+const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl ">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+      </p>
+      <button
+        className="btn-accent btn rounded-full  px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+};
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+// import { exampleRouter } from "~/server/api/routers/example";
 import { projectRouter } from "~/server/api/routers/project";
 import { createTRPCRouter } from "~/server/api/trpc";
 
@@ -8,7 +8,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  // example: exampleRouter,
   project: projectRouter,
 });
 
