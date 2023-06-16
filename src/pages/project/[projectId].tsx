269d5ac1154370b573f75b440c10ef49fe61e8fd
Anthony C <44069058+4nth0ny1@users.noreply.github.com>
2023-06-16 10:44:15 -0500

    createTodo (#4)
    Co-authored-by: Anthony Catullo <anthonycatullo@oatslam.local>

--- src/pages/project/[projectId].tsx
+++ src/pages/project/[projectId].tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 import { api } from "../../utils/api";
 import { useRouter } from "next/router";
 import { TodoList } from "../../components/todos/TodoList";
+import { CreateTodo } from "../../components/todos/CreateTodo";
 
 const SingleProjectPage: NextPage = () => {
   const router = useRouter();
@@ -22,6 +23,7 @@ const SingleProjectPage: NextPage = () => {
         <p>{data?.description}</p>
       </div>
       <div>
+        <CreateTodo projectId={projectId} />
         <TodoList />
       </div>
     </>
--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -7,27 +7,48 @@ import {
 } from "~/server/api/trpc";
 
 export const todoRouter = createTRPCRouter({
-    getAllTodos: protectedProcedure
-    .input(z.object({projectId: z.string().cuid()}))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.todo.findMany({
-        where: {
-          project: {
+  getAllTodos: protectedProcedure
+  .input(z.object({projectId: z.string().cuid()}))
+  .query(({ ctx, input }) => {
+    return ctx.prisma.todo.findMany({
+      where: {
+        project: {
+          id: input.projectId
+        }
+      },
+        orderBy: [{ createdAt: "desc" }],
+    });
+  }),
+
+  createTodo: protectedProcedure
+  .input(z.object({name: z.string(), description: z.string(), projectId: z.string()}))
+  .mutation(({ctx, input}) => {
+    return ctx.prisma.todo.create({
+      data: {
+        name: input.name,
+        description: input.description,
+        user: {
+          connect: {
+            id: ctx.session.user.id
+          }
+        }, 
+        project: {
+          connect: {
             id: input.projectId
           }
-        },
-          orderBy: [{ createdAt: "desc" }],
-      });
-    }),
-
-    deleteTodo: protectedProcedure
-    .input(z.string())
-    .mutation(({ctx, input}) => {
-      return ctx.prisma.todo.delete({
-        where: {
-          id: input
         }
-      })
+      }
     })
+  }), 
+
+  deleteTodo: protectedProcedure
+  .input(z.string())
+  .mutation(({ctx, input}) => {
+    return ctx.prisma.todo.delete({
+      where: {
+        id: input
+      }
+    })
+  })
 
 })
